1d26927073f6bf1139ab51b27420b5e8444ea51e
Eric Collazo <ericcollazo67@gmail.com>
2019-04-30 14:55:43 -0700

    Changed values.

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace dotnetcoreapi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Docker", "DotNet Core", "Linux-Windows-MacOS" };
         }
 
         // GET api/values/5
